56961878ee78a7478b6f20f6727448a03fa42ee7
mohammad <razappleid123@gmail.com>
2019-12-08 13:35:36 +0330

    create API Response

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 12/8/2019
+ * Time: 1:16 PM
+ */
+
+namespace App\Traits;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+
+trait ApiResponser
+{
+    private function successResponse($data, $code)
+    {
+        return response()->json($data, $code);
+    }
+
+    protected function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        $collection = $this->paginate($collection);
+        $collection = $this->cacheResponse($collection);
+        return $this->successResponse($collection, $code);
+    }
+
+    protected function showOne(Model $instance, $code = 200)
+    {
+        return $this->successResponse($instance, $code);
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50',
+        ];
+
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 10;
+
+        if (request()->has('per_page')) {
+            $perPage = (int)request()->per_page;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+
+        $queryParams = request()->query();
+        ksort($queryParams);
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30 / 60, function () use ($data) {
+            return $data;
+        });
+    }
+}
